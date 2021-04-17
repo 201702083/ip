d43331c006ae57c5dbaf19c6b180da39bb8694ac
201702083 <201702083@o.cnu.ac.kr>
2021-04-17 21:31:10 +0900

    DoG filter get

--- Week5/DoG.py
+++ Week5/DoG.py
@@ -4,29 +4,29 @@ import numpy as np
 # library add
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from my_gaussian import my_filtering
+from using_myfunc.my_gaussian import my_filtering
+from using_myfunc.my_gaussian import my_get_Gaussian2D_mask
 def get_DoG_filter(fsize, sigma=1):
     ###################################################
     # TODO                                            #
     # DoG mask 완성                                    #
     ###################################################
-    y, x = np.mgrid[-(fsize//2):1+(fsize//2),-(fsize//2):1+(fsize//2)]
+    b, a = np.mgrid[-(fsize//2):1+(fsize//2),-(fsize//2):1+(fsize//2)]
+    gaus = my_get_Gaussian2D_mask(fsize,sigma)
+    gaus = (( gaus - np.min(gaus))/ np.max(gaus - np.min(gaus)) * 255 ).astype(np.uint8)
 
-    # DoG_x = -(x / (sigma ** 2)) * (np.math.e ** (- (x * x + y * y) / (2 * sigma * sigma)))
-    # DoG_y = -(y / (sigma ** 2)) * (np.math.e ** (- (x * x + y * y) / (2 * sigma * sigma)))
-
-    DoG_x = (-x * (np.math.e ** (-(x*x+y*y) / (2*sigma*sigma))))
-    DoG_y = (-y * (np.math.e ** (-(x*x+y*y) / (2*sigma*sigma))))
-
-    # DoG_x = ((DoG_x - np.min(DoG_x)) / np.max(DoG_x - np.min(DoG_x)) * 255).astype(np.uint8)
-    # DoG_y = ((DoG_y - np.min(DoG_y)) / np.max(DoG_y - np.min(DoG_y)) * 255).astype(np.uint8)
+    DoG_x = (-b/sigma) * gaus
+    DoG_y = (-a/sigma) * gaus
+    DoG_x = DoG_x/np.max(DoG_x)
+    DoG_y = DoG_y/np.max(DoG_y)
 
     return DoG_x, DoG_y
 
 
 def main():
     src = cv2.imread('../imgs/Lena.png', cv2.IMREAD_GRAYSCALE)
-    print(src)
+    DoG_x, DoG_y = get_DoG_filter(fsize=3, sigma=1)
+
     ###################################################
     # TODO                                            #
     # DoG mask sigma값 조절해서 mask 만들기              #
@@ -35,24 +35,22 @@ def main():
     x, y = get_DoG_filter(fsize=256, sigma=30)
     # 256x256 sigma 30 의 x,y 미분값들
 
-    dst_x = my_filtering(src, x, 'zero')
-    dst_y = my_filtering(src, y, 'zero')
-    x = (x - np.min(x)) / np.max(x - np.min(x))
-    y = (y - np.min(y)) / np.max(y - np.min(y))
+    dst_x = my_filtering(src, DoG_x, 'zero')
+    dst_y = my_filtering(src, DoG_y, 'zero')
+    x = ((x - np.min(x)) / np.max(x - np.min(x))).astype(np.float32) # 0~1로 정규화
+    y = ((y - np.min(y)) / np.max(y - np.min(y))).astype(np.float32) # 0~1로 정규화
     ###################################################
     # TODO                                            #
     # dst_x, dst_y 를 사용하여 magnitude 계산            #
     ###################################################
-    # dst = np.sqrt(dst_x*dst_x + dst_y*dst_y)
+    dst = np.sqrt(dst_x*dst_x + dst_y*dst_y)
     cv2.imshow('original',src)
     cv2.imshow('DoG_x filter', x)
     cv2.imshow('DoG_y filter', y)
     cv2.imshow('dst_x', dst_x/255)
-    cv2.imshow('dst_x z ',dst_x)
     cv2.imshow('dst_y', dst_y/255)
-    cv2.imshow('dst_y z ',dst_y)
 
-    # cv2.imshow('dst', dst/255)
+    cv2.imshow('dst', dst/255)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
--- Week5/test.py
+++ Week5/test.py
@@ -4,25 +4,35 @@ import numpy as np
 # library add
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from my_gaussian import my_get_Gaussian2D_mask
-from my_filtering import my_filtering
-
+from using_myfunc.my_gaussian import my_get_Gaussian2D_mask
+from using_myfunc.my_gaussian import my_filtering
+print(30 * np.sqrt(np.math.e))
+src = cv2.imread('../imgs/sobel_test.png', cv2.IMREAD_GRAYSCALE)
+print(src)
+src_f = src.astype(np.float32)
 fsize = 256
 sigma = 30
-print(np.math.e)
-# y, x = np.mgrid[-(fsize // 2):1 + (fsize // 2), -(fsize // 2):1 + (fsize // 2)]
-#
-# DoG_x = -(x / (sigma ** 2)) * (np.math.e ** (- (x * x + y * y) / (2 * sigma * sigma)))
-# DoG_x = ((DoG_x - np.min(DoG_x)) / np.max(DoG_x - np.min(DoG_x))*255).astype(np.uint8)
-# DoG_y = -(y / (sigma ** 2)) * (np.math.e ** (- (x * x + y * y) / (2 * sigma * sigma)))
-# DoG_y = ((DoG_y - np.min(DoG_y)) / np.max(DoG_y - np.min(DoG_y))*255).astype(np.uint8)
-#
-# print('----------')
-# print(DoG_x)
-# print('----------')
-#
-# print(DoG_y)
-# cv2.imshow('DoG-x', DoG_x)
-# cv2.imshow('Dog-y', DoG_y)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+sb = np.dot([[-1,0,1]],my_get_Gaussian2D_mask(3,sigma))
+print(sb)
+der = np.array([[-1,0,1]])
+blur = np.array([[1],[2],[1]])
+sobel_x = np.dot(blur,der)
+sobel_y = np.dot(der.T,blur.T)
+print(np.sum(sobel_y),np.sum(sobel_x))
+
+
+dst_x = my_filtering(src,sobel_x,'zero')
+dst_y = my_filtering(src,sobel_y,'zero')
+dst_x = np.abs(dst_x)
+dst_y = np.abs(dst_y)
+dst = dst_x+dst_y
+xx = np.clip(dst_x,0,255).astype(np.uint8)
+yy = np.clip(dst_y,0,255).astype(np.uint8)
+cv2.imshow('xx',xx)
+cv2.imshow('yy',yy)
+cv2.imshow('dst',dst/255)
+cv2.imshow('dst_x',dst_x/255)
+cv2.imshow('dst_y',dst_y/255)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
