46d2bc1c54edbf2e4be5924614b0ff888a8cb29f
201702083 <201702083@o.cnu.ac.kr>
2021-05-09 22:04:04 +0900

    remove noise using bilateral filtering

--- Week8/bilateral.py
+++ Week8/bilateral.py
@@ -7,7 +7,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from my_library.padding import my_padding
 from my_library.filtering import my_filtering
-from my_library.get_filter import my_get_Gaussian2D_mask
+from my_library.my_gaussian import my_get_Gaussian2D_mask
 
 def my_normalize(src):
     dst = src.copy()
@@ -28,12 +28,28 @@ def my_bilateral(src, msize, sigma, sigma_r, pos_x, pos_y, pad_type='zero'):
     # TODO                                                                                             #
     # my_bilateral 완성                                                                                 #
     # mask를 만들 때 4중 for문으로 구현 시 감점(if문 fsize*fsize개를 사용해서 구현해도 감점) 실습영상 설명 참고      #
-    ####################################################################################################
-    (h, w) = src.shape
+    ###################################################################################################
 
+    # 거리에 대한 필터는 모든 마스크가 동일함.
+    (h, w) = src.shape
+    y, x = np.mgrid[-(msize//2):1+(msize//2),-(msize//2):1+(msize//2)]
+    Gmask = (1/(2*np.pi*(2*(sigma**2))))*np.exp(-(x ** 2 + y**2) / (2 * (sigma ** 2)))
+
+    # img_pad : 0 ~ 1
+    img_pad = my_padding(src,(msize//2,msize//2),pad_type)
+
+    # 결과 이미지 생성
+    dst = np.zeros(src.shape)
+
+    # for 문을 돌며 각 픽셀의 주변 부와의 컬러 차이를 계산하며 각자의 마스크를 계산함.
     for i in range(h):
         print('\r%d / %d ...' %(i,h), end="")
         for j in range(w):
+            Colordiff = src[i,j] - img_pad[i+y+msize//2 , j+x+msize//2]       # I(i,j) - I(i+y,j+x) -> 컬러 차이
+            color_mask = np.exp(- ( (Colordiff**2)/ (2*(sigma_r**2) ) ) )
+            mask = Gmask * color_mask
+            mask /= np.sum(mask)
+            #mask 의 총합 1
 
             if i==pos_y and j == pos_x:
                 print()
@@ -46,8 +62,7 @@ def my_bilateral(src, msize, sigma, sigma_r, pos_x, pos_y, pad_type='zero'):
                 img = cv2.resize(img, (200, 200), interpolation = cv2.INTER_NEAREST)
                 img = my_normalize(img)
                 cv2.imshow('img', img)
-
-            dst[i, j] = ??
+            dst[i, j] = np.sum(mask * img_pad[i:i+5,j:j+5])
 
     return dst
 
@@ -58,8 +73,8 @@ if __name__ == '__main__':
     src = cv2.imread('../imgs/Lena.png', cv2.IMREAD_GRAYSCALE)
     np.random.seed(seed=100)
 
-    pos_y = 0
-    pos_x = 0
+    pos_y = 65
+    pos_x = 453
     src_line = src.copy()
     src_line[pos_y-4:pos_y+5, pos_x-4:pos_x+5] = 255
     src_line[pos_y-2:pos_y+3, pos_x-2:pos_x+3] = src[pos_y-2:pos_y+3, pos_x-2:pos_x+3]
@@ -70,10 +85,10 @@ if __name__ == '__main__':
     # TODO                                               #
     # my_bilateral, gaussian mask 채우기                  #
     ######################################################
-    dst = my_bilateral(src_noise, 5, ??, ??, pos_x, pos_y)
+    dst = my_bilateral(src_noise, 5, 3, 0.1, pos_x, pos_y)
     dst = my_normalize(dst)
 
-    gaus2D = my_get_Gaussian2D_mask(5 , sigma = ??)
+    gaus2D = my_get_Gaussian2D_mask(5 , sigma = 1)
     dst_gaus2D= my_filtering(src_noise, gaus2D)
     dst_gaus2D = my_normalize(dst_gaus2D)
 
