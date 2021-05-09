c60a7640a5c8e16bc3d8663d0c0a976acc3d2652
201702083 <201702083@o.cnu.ac.kr>
2021-05-09 22:05:03 +0900

    Morphology convert

--- Week7/morphology.py
+++ Week7/morphology.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
+from my_library.padding import my_padding
 
 def dilation(B, S):
     ###############################################
     # TODO                                        #
     # dilation 함수 완성                           #
     ###############################################
+    h, w = B.shape
+    dst = np.zeros((h+2,w+2))
+    for row in range(h):
+        for col in range(w):
+            if ( B[row,col] == 1):
+                dst[row:row+3,col:col+3] = S
+    dst = dst[1:h + 2, 1:w + 2]
     return dst
 
 def erosion(B, S):
@@ -13,6 +21,16 @@ def erosion(B, S):
     # TODO                                        #
     # erosion 함수 완성                            #
     ###############################################
+    h,w = B.shape
+    pad_img = my_padding(B,(1,1),'zero')
+    dst = np.zeros(pad_img.shape)
+    for row in range(h):
+        for col in range(w):
+            if ( np.array_equal( pad_img[row:row+3,col:col+3], S)):
+                dst[row+1,col+1] = 1
+
+    dst = dst[1:h+1,1:w+1]
+
     return dst
 
 def opening(B, S):
@@ -20,13 +38,15 @@ def opening(B, S):
     # TODO                                        #
     # opening 함수 완성                            #
     ###############################################
+    dst = dilation(erosion(B,S),S)
     return dst
 
 def closing(B, S):
     ###############################################
     # TODO                                        #
     # closing 함수 완성                            #
-    ###############################################
+    ###############################################/
+    dst = erosion(dilation(B,S),S)
     return dst
 
 
