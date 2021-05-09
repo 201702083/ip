d0f651794ab8027d67c29cb7b4b123c7d7644946
201702083 <201702083@o.cnu.ac.kr>
2021-05-09 22:02:51 +0900

    Morphology convert

---
+++ Week7/morphology.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+def dilation(B, S):
+    ###############################################
+    # TODO                                        #
+    # dilation 함수 완성                           #
+    ###############################################
+    return dst
+
+def erosion(B, S):
+    ###############################################
+    # TODO                                        #
+    # erosion 함수 완성                            #
+    ###############################################
+    return dst
+
+def opening(B, S):
+    ###############################################
+    # TODO                                        #
+    # opening 함수 완성                            #
+    ###############################################
+    return dst
+
+def closing(B, S):
+    ###############################################
+    # TODO                                        #
+    # closing 함수 완성                            #
+    ###############################################
+    return dst
+
+
+if __name__ == '__main__':
+    B = np.array(
+        [[0, 0, 0, 0, 0, 0, 0, 0],
+         [1, 1, 1, 1, 1, 1, 1, 0],
+         [0, 0, 0, 1, 1, 1, 1, 0],
+         [0, 0, 0, 1, 1, 1, 1, 0],
+         [0, 0, 1, 1, 1, 1, 1, 0],
+         [0, 0, 0, 1, 1, 1, 1, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0]])
+
+    S = np.array(
+        [[1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1]])
+
+
+    cv2.imwrite('morphology_B.png', (B*255).astype(np.uint8))
+
+    img_dilation = dilation(B, S)
+    img_dilation = (img_dilation*255).astype(np.uint8)
+    print(img_dilation)
+    cv2.imwrite('morphology_dilation.png', img_dilation)
+
+    img_erosion = erosion(B, S)
+    img_erosion = (img_erosion * 255).astype(np.uint8)
+    print(img_erosion)
+    cv2.imwrite('morphology_erosion.png', img_erosion)
+
+    img_opening = opening(B, S)
+    img_opening = (img_opening * 255).astype(np.uint8)
+    print(img_opening)
+    cv2.imwrite('morphology_opening.png', img_opening)
+
+    img_closing = closing(B, S)
+    img_closing = (img_closing * 255).astype(np.uint8)
+    print(img_closing)
+    cv2.imwrite('morphology_closing.png', img_closing)
+
+
